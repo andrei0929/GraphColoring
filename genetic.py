41623d4669ecc9ffb9045a901f070b459138beeb
Andrei Oltean <andrei.oltean@tapptitude.com>
2018-07-09 00:21:35 +0300

    Add 2 types of crossover (random gene replacing crossover and partially mapped crossover)

--- genetic.py
+++ genetic.py
@@ -1,7 +1,7 @@
 import random
-import statistics
-import sys
-import time
+from bisect import bisect_left
+from enum import Enum
+from math import exp
 
 
 def _generate_parent(length, geneSet, get_fitness):
@@ -20,7 +20,7 @@ def _generate_parent(length, geneSet, get_fitness):
         sampleSize = min(length - len(genes), len(geneSet))
         genes.extend(random.sample(geneSet, sampleSize))
     fitness = get_fitness(genes)
-    return Chromosome(genes, fitness)
+    return Chromosome(genes, fitness, Strategies.Create)
 
 
 def _mutate(parent, geneSet, get_fitness):
@@ -38,7 +38,7 @@ def _mutate(parent, geneSet, get_fitness):
     newGene, alternate = random.sample(geneSet, 2)
     childGenes[index] = alternate if newGene == childGenes[index] else newGene
     fitness = get_fitness(childGenes)
-    return Chromosome(childGenes, fitness)
+    return Chromosome(childGenes, fitness, Strategies.Mutate)
 
 
 def _mutate_custom(parent, custom_mutate, get_fitness):
@@ -54,11 +54,26 @@ def _mutate_custom(parent, custom_mutate, get_fitness):
     childGenes = parent.Genes[:]
     custom_mutate(childGenes)
     fitness = get_fitness(childGenes)
-    return Chromosome(childGenes, fitness)
+    return Chromosome(childGenes, fitness, Strategies.Mutate)
+
+
+def _crossover(parentGenes, index, parents, get_fitness, crossover, mutate,
+               generate_parent):
+    donorIndex = random.randrange(0, len(parents))
+    if donorIndex == index:
+        donorIndex = (donorIndex + 1) % len(parents)
+    childGenes = crossover(parentGenes, parents[donorIndex].Genes)
+    if childGenes is None:
+        # parent and donor are indistinguishable
+        parents[donorIndex] = generate_parent()
+        return mutate(parents[index])
+    fitness = get_fitness(childGenes)
+    return Chromosome(childGenes, fitness, Strategies.Crossover)
 
 
 def get_best(get_fitness, targetLen, optimalFitness, geneSet, display,
-             custom_mutate=None):
+             custom_mutate=None, custom_create=None, maxAge=None,
+             poolSize=1, crossover=None):
     """
 
     Function which computes the best child which is of optimalFitness
@@ -69,9 +84,14 @@ def get_best(get_fitness, targetLen, optimalFitness, geneSet, display,
     :param optimalFitness: optimal fitness value
     :param geneSet: set of possible genes
     :param display: display function
+    :param custom_create: custom initial creation function
     :param custom_mutate: custom mutation function
+    :param maxAge: maximum age after which the genetic line dies
+    :param poolSize: number of parents for a child
+    :param crossover: crossover function
     :return: best child with fitness value equal to optimalFitness
     """
+
     if custom_mutate is None:
         def fnMutate(parent):
             return _mutate(parent, geneSet, get_fitness)
@@ -79,16 +99,42 @@ def get_best(get_fitness, targetLen, optimalFitness, geneSet, display,
         def fnMutate(parent):
             return _mutate_custom(parent, custom_mutate, get_fitness)
 
-    def fnGenerateParent():
-        return _generate_parent(targetLen, geneSet, get_fitness)
+    if custom_create is None:
+        def fnGenerateParent():
+            return _generate_parent(targetLen, geneSet, get_fitness)
+    else:
+        def fnGenerateParent():
+            genes = custom_create()
+            return Chromosome(genes, get_fitness(genes), Strategies.Create)
 
-    for improvement in _get_improvement(fnMutate, fnGenerateParent):
+    strategyLookup = {
+        Strategies.Create: lambda p, i, o: fnGenerateParent(),
+        Strategies.Mutate: lambda p, i, o: fnMutate(p),
+        Strategies.Crossover: lambda p, i, o:
+        _crossover(p.Genes, i, o, get_fitness, crossover, fnMutate,
+                   fnGenerateParent)
+    }
+
+    usedStrategies = [strategyLookup[Strategies.Mutate]]
+    if crossover is not None:
+        usedStrategies.append(strategyLookup[Strategies.Crossover])
+
+        def fnNewChild(parent, index, parents):
+            return random.choice(usedStrategies)(parent, index, parents)
+    else:
+        def fnNewChild(parent, index, parents):
+            return fnMutate(parent)
+
+    for improvement in _get_improvement(fnNewChild, fnGenerateParent,
+                                        maxAge, poolSize):
         display(improvement)
+        f = strategyLookup[improvement.Strategy]
+        usedStrategies.append(f)
         if not optimalFitness > improvement.Fitness:
             return improvement
 
 
-def _get_improvement(new_child, generate_parent):
+def _get_improvement(new_child, generate_parent, maxAge, poolSize):
     """
 
     Generator function which successively computes a mutation of the best previous guess
@@ -96,22 +142,66 @@ def _get_improvement(new_child, generate_parent):
 
     :param new_child: function which states how should a new child be computed
     :param generate_parent: function which generated the first initial guess
+    :param maxAge: maximum age after which the genetic line dies
+    :param poolSize: number of parents of a child
     :return: the guess with the larger fitness value
     """
+
     bestParent = generate_parent()
     yield bestParent
+    parents = [bestParent]
+    historicalFitnesses = [bestParent.Fitness]
+    for _ in range(poolSize - 1):
+        parent = generate_parent()
+        if parent.Fitness > bestParent.Fitness:
+            yield parent
+            bestParent = parent
+            historicalFitnesses.append(parent.Fitness)
+        parents.append(parent)
+    lastParentIndex = poolSize - 1
+    pindex = 1
     while True:
-        child = new_child(bestParent)
-        if bestParent.Fitness > child.Fitness:
+        pindex = pindex - 1 if pindex > 0 else lastParentIndex
+        parent = parents[pindex]
+        child = new_child(parent, pindex, parents)
+        if parent.Fitness > child.Fitness:
+            if maxAge is None:
+                continue
+            parent.Age += 1
+            if maxAge > parent.Age:
+                continue
+            index = bisect_left(historicalFitnesses, child.Fitness, 0,
+                                len(historicalFitnesses))
+            difference = len(historicalFitnesses) - index
+            proportionSimilar = difference / len(historicalFitnesses)
+            if random.random() < exp(-proportionSimilar):
+                parents[pindex] = child
+                continue
+            bestParent.Age = 0
+            parents[pindex] = bestParent
             continue
-        if not child.Fitness > bestParent.Fitness:
+        if not child.Fitness > parent.Fitness:
+            # same fitness
+            child.Age = parent.Age + 1
+            parents[pindex] = child
+            continue
+        child.Age = 0
+        parents[pindex] = child
+        if child.Fitness > bestParent.Fitness:
             bestParent = child
-            continue
-        yield child
-        bestParent = child
+            yield bestParent
+            historicalFitnesses.append(bestParent.Fitness)
 
 
 class Chromosome:
-    def __init__(self, genes, fitness):
+    def __init__(self, genes, fitness, strategy):
         self.Genes = genes
         self.Fitness = fitness
+        self.Strategy = strategy
+        self.Age = 0
+
+
+class Strategies(Enum):
+    Create = 0,
+    Mutate = 1,
+    Crossover = 2
---
+++ graphColoring.py
@@ -0,0 +1,147 @@
+import random
+import unittest
+import datetime
+from itertools import chain
+
+import genetic
+
+
+def load_data(localFileName):
+    rules = set()
+    nodes = set()
+    with open(localFileName, mode='r') as infile:
+        content = infile.read().splitlines()
+    for row in content:
+        if row[0] == 'e':  # e aa bb, aa and bb are node ids
+            nodeIds = row.split(' ')[1:3]
+            rules.add(Rule(nodeIds[0], nodeIds[1]))
+            nodes.add(nodeIds[0])
+            nodes.add(nodeIds[1])
+            continue
+        if row[0] == 'n':  # n aa ww, aa is a node id, ww is a weight
+            nodeIds = row.split(' ')
+            nodes.add(nodeIds[1])
+    return rules, nodes
+
+
+def get_fitness(genes, rules, stateIndexLookup):
+    rulesThatPass = sum(1 for rule in rules
+                        if rule.isValid(genes, stateIndexLookup))
+    return rulesThatPass
+
+
+def display(candidate, startTime):
+    timeDiff = datetime.datetime.now() - startTime
+    print("{0}\t{1}\t{2}".format(
+        ''.join(map(str, candidate.Genes)),
+        candidate.Fitness,
+        str(timeDiff)))
+
+
+def mutate(genes, geneset):
+    # mutate between 1 and 4 genes
+    count = random.randint(1, 4)
+    while count > 0:
+        indexA = random.randrange(0, len(genes))
+        indexNewGeneA = random.randrange(0, len(geneset))
+        genes[indexA] = geneset[indexNewGeneA]
+        count -= 1
+
+
+def crossover2(parent, otherParent):
+    childGenes = parent[:]
+    if len(parent) <= 2 or len(otherParent) < 2:
+        return childGenes
+    length = random.randint(1, len(parent) - 2)
+    start = random.randrange(0, len(parent) - length)
+    childGenes[start:start + length] = otherParent[start:start + length]
+    return childGenes
+
+
+def crossover(parent, otherParent):
+    childGenes = otherParent[:]
+    if len(parent) <= 2 or len(otherParent) < 2:
+        return childGenes
+    length = random.randint(1, len(parent) - 2)
+    start = random.randrange(0, len(parent) - length)
+    childGenes[start:start + length] = parent[start:start + length]
+    for i in range(start, start + length):
+        if otherParent[i] not in childGenes[start:start + length]:
+            j = i
+            initialValue = otherParent[j]
+            while parent[j] in otherParent[start:start + length] and j in range(start, start + length):
+                j = otherParent.index(parent[j])
+            childGenes[otherParent.index(parent[j])] = initialValue
+    return childGenes
+
+
+class GraphColoringTests(unittest.TestCase):
+
+    def test_states(self):
+        self.color("adjacent_states.col", ["Orange", "Yellow", "Green",
+                                           "Blue"])
+
+    def test_R100_1gb(self):
+        self.color("R100_1gb.col",
+                   ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo"])
+
+    def color(self, file, colors):
+        rules, nodes = load_data(file)
+        optimalValue = len(rules)
+        colorLookup = {color[0]: color for color in colors}
+        geneset = list(colorLookup.keys())
+        startTime = datetime.datetime.now()
+        nodeIndexLookup = {key: index
+                           for index, key in enumerate(sorted(nodes))}
+
+        def fnDisplay(candidate):
+            display(candidate, startTime)
+
+        def fnGetFitness(genes):
+            return get_fitness(genes, rules, nodeIndexLookup)
+
+        def fnMutate(genes):
+            mutate(genes, geneset)
+
+        def fnCrossover(parent, donor):
+            return crossover2(parent, donor)
+
+        best = genetic.get_best(fnGetFitness, len(nodes), optimalValue, geneset, fnDisplay, custom_mutate=fnMutate,
+                                maxAge=500, poolSize=25, crossover=fnCrossover)
+        self.assertTrue(not optimalValue > best.Fitness)
+
+        keys = sorted(nodes)
+        for index in range(len(nodes)):
+            print(keys[index] + " is " + colorLookup[best.Genes[index]])
+
+
+class Rule:
+    Node = None
+    Adjacent = None
+
+    def __init__(self, node, adjacent):
+        if node < adjacent:
+            node, adjacent = adjacent, node
+        self.Node = node
+        self.Adjacent = adjacent
+
+    def __eq__(self, other):
+        return self.Node == other.Node and \
+               self.Adjacent == other.Adjacent
+
+    def __hash__(self):
+        return hash(self.Node) * 397 ^ hash(self.Adjacent)
+
+    def __str__(self):
+        return self.Node + " -> " + self.Adjacent
+
+    # Verify if genes from 2 adjacent nodes are identical (colored with the same color)
+    def isValid(self, genes, nodeIndexLookup):
+        index = nodeIndexLookup[self.Node]
+        adjacentStateIndex = nodeIndexLookup[self.Adjacent]
+
+        return genes[index] != genes[adjacentStateIndex]
+
+
+if __name__ == '__main__':
+    unittest.main()
